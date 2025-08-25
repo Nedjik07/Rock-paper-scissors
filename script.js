06d4212fef89785ab53174d434bd80a2308b7fd4
Nedjik07 <Nedjik07@gmail.com>
2025-08-26 00:33:43 +0200

    Define basic plan for ComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function getComputerChoice() {
-    
+    // Thi function will use Math.random() to choose a number between 0 and 1
+    // We will then multiply to 10 so we have Integers
+    // We then will use Math.float() to float to the precedent digit
+    // Then since it include 0 we will have 9 possibilities
+    // We can then divide by 3 -> 0-2, 3-5, 6-8
+    // If the number is between 0-2 choose Rock, if between 3-5 choose Paper...Accordingly with scissor
+    // Return the value
 }
+    
