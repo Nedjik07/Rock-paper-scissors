4ce2854fa9b5ac6c1deb636b612be78297466013
Nedjik07 <Nedjik07@gmail.com>
2025-08-27 00:27:35 +0200

    Trying to solve the playRoung bug

--- script.js
+++ script.js
@@ -22,23 +22,23 @@ function getHumanChoice() {
 function playRound(computerChoice, humanChoice) {
     computerChoice = getComputerChoice(); // Store computerChoice
     humanChoice = getHumanChoice().toLocaleLowerCase(); // Store humanChoice and make it case insensitive
-    if (humanChoice === "rock" && computerChoice == "Scissors") {
+    if ((humanChoice === "rock") && (computerChoice == "Scissors")) {
         console.log("You win! Rock beats Scissors");
-    } else if (humanChoice === "rock" && computerChoice === "Paper") {
+    } else if ((humanChoice === "rock") && (computerChoice === "Paper")) {
         console.log("You lose! Paper beats Rock!");
-    } else if (humanChoice === "rock" && computerChoice === "Rock") {
+    } else if ((humanChoice === "rock") && (computerChoice === "Rock")) {
         console.log("Draw!");
-    } else if (humanChoice === "paper" && computerChoice === "Rock") {
+    } else if ((humanChoice === "paper") && (computerChoice === "Rock")) {
         console.log("You win! Paper beats Rock!");
-    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
+    } else if ((humanChoice === "paper") && (computerChoice === "Scissors")) {
         console.log("You lose! Scissors beats Paper!");
-    } else if (humanChoice === "paper" && computerChoice === "Paper") {
+    } else if ((humanChoice === "paper") && (computerChoice === "Paper")) {
         console.log("Draw!");
-    } else if (humanChoice === "scissors" && computerChoice === "Paper") {
+    } else if ((humanChoice === "scissors") && (computerChoice === "Paper")) {
         console.log("You win! Scissors beats Paper");
-    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
+    } else if ((humanChoice === "scissors") && (computerChoice === "Rock")) {
         console.log("You lose! Rock beats Scissors!");
-    } else (humanChoice === "scissors" && computerChoice === "Scissors")
+    } else ((humanChoice === "scissors") && (computerChoice === "Scissors")) // works
         console.log("Draw!");
 }
 playRound();
