91a1d2f18dbcd5838181aac53c9d4fbb655be2ab
Nedjik07 <Nedjik07@gmail.com>
2025-08-26 20:09:45 +0200

    Write playRound structure, need fix

--- script.js
+++ script.js
@@ -13,14 +13,39 @@ function getComputerChoice(n) {
         return "Scissors";
     }
 }
+alert(getComputerChoice());
 
 function getHumanChoice() {
     return prompt("Choose your weapon!: "); // This use a prompt to ask user about their choice and returns the value
 }
 
-function playRound(humanChoice, computerChoice) {
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice().toLocaleLowerCase();
-    
+function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice(); // Store computerChoice
+    humanChoice = getHumanChoice().toLocaleLowerCase(); // Store humanChoice and make it case insensitive
+    if (humanChoice === "rock") {
+        if (computerChoice === "Scissors") {
+            console.log("You win! Rock beats Scissors");
+        } else if (computerChoice === "Paper") {
+            console.log("You lose! Paper beats Rock!");
+        } else {
+            console.log("Draw!");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "Rock") {
+            console.log("You win! Paper beats Rock!");
+        } else if (computerChoice === "Scissors") {
+            console.log("You lose! Scissors beats Paper!");
+        } else {
+            console.log("Draw!");
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "Paper") {
+            console.log("You win! Scissors beats Paper");
+        } else if (computerChoice === "Rock") {
+            console.log("You lose! Rock beats Scissors!");
+        } else {
+            console.log("Draw!");
+        }
+    }
 }
 console.log(playRound());
