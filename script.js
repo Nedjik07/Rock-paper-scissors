9a5bc47acd1af1bad831dbe8152b52ef80ec1a97
Nedjik07 <Nedjik07@gmail.com>
2025-08-26 19:30:01 +0200

    Simplify both functions by also removing unnecessary variables

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-function getComputerChoice() {
+let humanScore, computerScore = 0;
+
+
+function getComputerChoice(n) {
     // Choose a number between 0, 1, 2 with Math.floor and Math.random
     // Use a condition to return between Rock, Paper, or Scissors
-
-    let computerChoice = Math.floor(Math.random() * 3); // Simplified
-
-    if (computerChoice === 0) {
+    n = Math.floor(Math.random() * 3); // Simplified
+    if (n === 0) {
         return "Rock";
-    } else if (computerChoice === 1) {
+    } else if (n === 1) {
         return "Paper";
-    } else if (computerChoice === 2) {
+    } else if (n === 2) {
         return "Scissors";
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose your weapon!: ");
-    return humanChoice;
+    return prompt("Choose your weapon!: "); // This use a prompt to ask user about their choice and returns the value
 }
-console.log(getHumanChoice());
+
+function playRound() {
+
+}
