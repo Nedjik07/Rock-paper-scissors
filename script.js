f576a67c73c3a027ad808fd132d98f442a963575
Nedjik07 <Nedjik07@gmail.com>
2025-08-26 18:36:16 +0200

    Simplify function logic structure and write the code for ComputerChoice

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function getComputerChoice() {
-    // Thi function will use Math.random() to choose a number between 0 and 1
-    // We will then multiply to 10 so we have Integers
-    // We then will use Math.float() to float to the precedent digit
-    // Then since it include 0 we will have 9 possibilities
-    // We can then divide by 3 -> 0-2, 3-5, 6-8
-    // If the number is between 0-2 choose Rock, if between 3-5 choose Paper...Accordingly with scissor
-    // Return the value
+    // Choose a number between 0, 1, 2 with Math.floor and Math.random
+    // Use a condition to return between Rock, Paper, or Scissors
+
+    let computerChoice = Math.floor(Math.random() * 3); // Simplified
+
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else if (computerChoice === 2) {
+        return "Scissors";
+    }
 }
-    
+console.log(getComputerChoice());
