8b1b4dfbbdb221c39d46e103e71b312c02090bae
Nedjik07 <Nedjik07@gmail.com>
2026-01-29 14:00:09 +0100

    Add the buttons and the choice logic and remove playRound logic

--- script.js
+++ script.js
@@ -2,59 +2,35 @@ let humanScore = 0;
 let computerScore = 0;
 let numberRound = 1;
 
-
+// Computer choice
 function getComputerChoice(n) {
     // Choose a number between 0, 1, 2 with Math.floor and Math.random
     // Use a condition to return between Rock, Paper, or Scissors
     n = Math.floor(Math.random() * 3); // Simplified
     if (n === 0) {
-        return "Rock";
+        return "rock";
     } else if (n === 1) {
-        return "Paper";
+        return "paper";
     } else if (n === 2) {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-function getHumanChoice() {
-    return prompt("Choose your weapon!"); // This use a prompt to ask user about their choice and returns the value
-}
-
+// Play function
 function playRound(computerChoice, humanChoice) {
     computerChoice = getComputerChoice().toLocaleLowerCase(); // Store computerChoice
-    humanChoice = getHumanChoice().toLocaleLowerCase(); // Store humanChoice and make it case insensitive
-   
-    // Console.log(computerChoice + " computer"); // This is just a test
-    // Console.log(humanChoice + " human"); // This is also a test
-    
-    // Game randomness condition
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You win! Rock beats Scissors" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You lose! Paper beats Rock!" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You win! Paper beats Rock!" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You lose! Scissors beats Paper!" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You win! Scissors beats Paper" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore ++;
-        alert("Round number " +numberRound + '\n' + '\n' + "You lose! Rock beats Scissors!" + '\n' + '\n' + "Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    } else {
-        alert("Round number " +numberRound + '\n' + '\n' + "Draw!" + '\n' +"Your score: " + humanScore + '\n' + "Computer score: " + computerScore);
-    }   
-    numberRound ++;
 }
 
-function playGame() {
-    for (i = 0; i < 5; i++) {
-        playRound();
-        // alert("Your score: " + humanScore + '\n' + "Computer score: " + computerScore); // Show score
-    }
-}
+
+const btns = document.querySelectorAll('button')
+
+btns.forEach(btn => {
+   btn.onclick = () => {
+    alert(btn.id);
+    
+    
+    const computerSelection = getComputerChoice();
+    playRound(computerSelection, btn.id);
+    alert(computerSelection);
+   }
+});
